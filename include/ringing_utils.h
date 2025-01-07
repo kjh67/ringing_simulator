46f79d624648999c6d8e441d6d5063fb45d713d5
K Hawkins <kjh67@cantab.ac.uk>
2025-01-07 10:56:11 +0000

    Implemented Method and MethodClass.

--- include/ringing_utils.h
+++ include/ringing_utils.h
@@ -1,3 +1,6 @@
+#include <string>
+
+using std::string;
 namespace ringing_utils {
 
 class Row
@@ -9,10 +12,31 @@ class Row
 };
 
 
-class Method
+enum MethodClass {
+    NONE=0, PLACE, BOB, SLOW_COURSE, TREBLE_BOB, DELIGHT, SURPRISE, ALLIANCE, TREBLE_PLACE, HYBRID, UNCLASSED
+};
+string MethodClassToString(MethodClass m);
+MethodClass StringToMethodClass(string classification);
+
+struct Method
 {
-    public:
-        //methods
+    string title;
+    string id;
+    int stage;
+
+    MethodClass classification;
+    bool little;
+    bool differential;
+    bool plain;
+    bool treble_dodging;
+    
+    string place_notation;
+
+    int lead_length;
+    int num_hunt_bells;
+    //TODO: change from string to Row once Row has been implemented
+    string lead_head;
+    string lead_head_code;
 };
 
 
--- src/ringing_utils/method.cpp
+++ src/ringing_utils/method.cpp
@@ -1,8 +1,69 @@
-// Implementation for the Method class 
+#include <string>
+#include <cstring>
 #include "ringing_utils.h"
 
+using std::string;
 namespace ringing_utils {
-    
+
+string MethodClassToString(MethodClass m) {
+    switch (m)
+    {
+    case PLACE:
+        return "Place";
+    case BOB:
+        return "Bob";
+    case SLOW_COURSE:
+        return "Slow Course";
+    case TREBLE_BOB:
+        return "Treble Bob";
+    case DELIGHT:
+        return "Delight";
+    case SURPRISE:
+        return "Surprise";
+    case ALLIANCE:
+        return "Alliance";
+    case TREBLE_PLACE:
+        return "Treble Place";
+    case HYBRID:
+        return "Hybrid";
+    case UNCLASSED:
+        return "";
+    default:
+        return "";
+    };
 }
 
-//place notation, calls, how often the calls are, etc
+MethodClass StringToMethodClass(string classification) {
+    if (strcmp(classification.c_str(), "Place") == 0) {
+        return PLACE;
+    }
+    else if (strcmp(classification.c_str(), "Bob") == 0) {
+        return BOB;
+    }
+    else if (strcmp(classification.c_str(), "Slow Course") == 0) {
+        return SLOW_COURSE;
+    }
+    else if (strcmp(classification.c_str(), "Treble Bob") == 0) {
+        return TREBLE_BOB;
+    }
+    else if (strcmp(classification.c_str(), "Delight") == 0) {
+        return DELIGHT;
+    }
+    else if (strcmp(classification.c_str(), "Surprise") == 0) {
+        return SURPRISE;
+    }
+    else if (strcmp(classification.c_str(), "Alliance") == 0) {
+        return ALLIANCE;
+    }
+    else if (strcmp(classification.c_str(), "Treble Place") == 0) {
+        return TREBLE_PLACE;
+    }
+    else if (strcmp(classification.c_str(), "Hybrid") == 0) {
+        return HYBRID;
+    }
+    else {
+        return UNCLASSED;
+    }
+}
+
+}
